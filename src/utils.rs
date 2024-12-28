769652ccf9921355d0fcae57a933cf411c31ee4e
stefanlight8 <64615032+stefanlight8@users.noreply.github.com>
2024-12-28 05:29:47 +0300

    Replace `walkdir` dependency with function

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,25 @@
+use std::fs::read_dir;
+use std::io::Error;
+use std::path::PathBuf;
+
+pub fn walk_dir(path: PathBuf) -> Result<Vec<PathBuf>, Error> {
+    let mut files = vec![];
+    let mut visit = vec![path];
+    while let Some(path) = visit.pop() {
+        for dir in read_dir(path)? {
+            let entry = dir?;
+            let file_type = entry.file_type()?;
+            if file_type.is_file() {
+                files.push(entry.path());
+            } else {
+                visit.push(entry.path())
+            }
+        }
+    }
+
+    Ok(files)
+}
+
 pub fn humanize_bytes(bytes: usize) -> String {
     let sizes = ["B", "KB", "MB", "GB", "TB"];
     let mut byte_size = bytes as f64;
